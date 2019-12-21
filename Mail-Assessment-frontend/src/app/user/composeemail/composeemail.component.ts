80c7ac23d4f885ef6c5138b3722f33ee6c91c759
lomadisireesha <lomadisireesha@gmail.com>
2019-12-21 18:22:19 +0530

    Mail-Assessment-frontend/.editorconfig

---
+++ Mail-Assessment-frontend/src/app/user/composeemail/composeemail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-composeemail',
+  templateUrl: './composeemail.component.html',
+  styleUrls: ['./composeemail.component.css']
+})
+export class ComposeemailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
